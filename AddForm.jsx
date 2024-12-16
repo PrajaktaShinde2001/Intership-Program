eaa68d62e8d28196ac6e34359b683bf9de19e65b
PrajaktaShinde2001 <prajaktashinde389@gmail.com>
2024-12-16 19:04:42 +0530

    Add files via upload

---
+++ AddForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AddFrom(){
+    return(
+        <form>
+            <h1>Students List</h1>
+            <label>
+                Register yourself:
+                <input type="text" />
+            </label>
+        </form>
+    );
+}
+export default AddForm;
