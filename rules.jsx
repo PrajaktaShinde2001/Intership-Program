1bc68bf0980c16b3efa9cefbde373fe65a1e3388
PrajaktaShinde2001 <prajaktashinde389@gmail.com>
2025-01-05 20:05:52 +0530

    Add files via upload

---
+++ rules.jsx
@@ -0,0 +1,9 @@
+// close all tags
+// use camelcase for attributes, class -> className, for -> htmlFor
+// using a single root element : each jsx expression must return single root element
+const element=(
+    <div class="container">
+        <h1>Hello</h1>
+        <img src="" alt="" />
+    </div>
+)
