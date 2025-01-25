e3e6361fb2533b5875d24707bb3ef807944bcec8
PrajaktaShinde2001 <prajaktashinde389@gmail.com>
2025-01-25 18:54:01 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className="container text-center mt-5">
+      <h1>Welcome to JobRecruit</h1>
+      <p>Your gateway to finding the best job opportunities!</p>
+    </div>
+  );
+};
+
+export default Home;
